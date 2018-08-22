34b250aba48c1f7afe42ead4f67e49a3da6c14c4
Matan Tsuberi <dev.matan.tsuberi@gmail.com>
2018-08-22 14:37:12 +0300

    add conviniece `lockId` methods

---
+++ contracts/Protected.sol
@@ -0,0 +1,247 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+/**
+ * @title Protected
+ * @dev base class that gives contracts a sophisticated access control mechanism
+ */
+contract Protected {
+    using SafeMath for uint;
+
+    // Random placeholder for irrelevent params in lock _id. e.g. `unlock(keccak256(abi.encodePacked("method", param1, ANYTHING, param2)))`
+    uint internal constant ANYTHING = uint(keccak256("ANYTHING"));
+
+    struct Key {
+        bool exists;
+        bool assignable;
+        uint expiration; // zero = no expiration
+        uint uses; // zero = infinite uses
+    }
+
+    //      id                 owner      key
+    mapping(bytes32 => mapping(address => Key)) public keys;
+
+    event AssignKey(
+        bytes32 indexed _id,
+        address indexed _from, // zero = granted by contract
+        address indexed _to,
+        bool _assignable,
+        uint _expiration,
+        uint _uses
+    );
+    event RevokeKey(
+        bytes32 indexed _id,
+        address indexed _owner
+    );
+
+
+    /**
+     * @dev is the current block timestamp less than `_expiration`
+     * @param _expiration expiration block timestamp
+     * @return is the expiration valid
+     */
+    function isValidExpiration(uint _expiration) public view returns (bool valid) {
+        // solium-disable-next-line security/no-block-members
+        return _expiration == 0 || _expiration >= now;
+    }
+
+    /**
+     * @dev does the owner have a valid key for the lock id
+     * @param _id lock id
+     * @param _owner owner address
+     */
+    function unlockable(bytes32 _id, address _owner) public view returns (bool) {
+        Key memory key = keys[_id][_owner];
+        return key.exists && isValidExpiration(key.expiration);
+    }
+
+    /**
+     * @dev transfer partial or all capabilities from the sender to an account
+     * @param _id lock id
+     * @param _to recipient
+     * @param _assignable can the recipient further assignKey capabilities to other accounts?
+     * @param _expiration the key's expiration time (block timestamp)
+     * @param _uses number of times this key can be used (in `unlock(..)`)
+     */
+    function assignKey(
+        bytes32 _id,
+        address _to,
+        bool _assignable,
+        uint _expiration,
+        uint _uses
+    ) public
+    {
+        Key memory key = keys[_id][msg.sender];
+        require(key.exists && isValidExpiration(key.expiration), "Invalid key");
+        require(key.assignable, "Key is not assignable");
+        require(key.expiration == 0 || (_expiration <= key.expiration && _expiration > 0), "Cannot extend key's expiration");
+        require(key.uses == 0 || (_uses <= key.uses && _uses > 0), "Not enough uses avaiable");
+        require(isValidExpiration(_expiration), "Expiration must be in the future");
+
+        require(
+            !unlockable(_id, _to) || (keys[_id][_to].assignable == _assignable && keys[_id][_to].expiration == _expiration),
+            "Cannot merge into recepeint's key"
+        );
+
+        subtractUses(_id,_uses);
+
+        if (unlockable(_id, _to)) {
+            if (keys[_id][_to].uses != 0) {
+                if (_uses == 0) {
+                    keys[_id][_to].uses = 0;
+                } else {
+                    keys[_id][_to].uses = keys[_id][_to].uses.add(_uses);
+                }
+            }
+        } else {
+            keys[_id][_to] = Key(true, _assignable, _expiration, _uses);
+        }
+
+        emit AssignKey(
+            _id,
+            msg.sender,
+            _to,
+            _assignable,
+            _expiration,
+            _uses
+        );
+    }
+
+    /**
+     * @dev transfer all capabilities from the sender to an account
+     * @param _id lock id
+     * @param _to recipient
+     */
+    function assignFullKey(
+        bytes32 _id,
+        address _to
+    ) public
+    {
+        Key memory key = keys[_id][_to];
+        assignKey(
+            _id,
+            _to,
+            key.assignable,
+            key.expiration,
+            key.uses
+        );
+    }
+
+    /**
+     * @dev revoke the sender's key
+     * @param _id lock id
+     */
+    function revokeKey(bytes32 _id) public {
+        revokeOwnerKey(_id, msg.sender);
+    }
+
+    /**
+     * @dev Revokes an account's key
+     * @param _id lock id
+     * @param _owner the account address
+     */
+    function revokeOwnerKey(bytes32 _id, address _owner) internal {
+        delete keys[_id][_owner];
+        emit RevokeKey(_id, _owner);
+    }
+
+    /**
+     * @dev Grant capabilities to account (overwrites existing key)
+     * @param _id lock id
+     * @param _to recipient
+     * @param _assignable can the recipient further assignKey his capabilities to other accounts?
+     * @param _expiration the key's expiration time (block timestamp)
+     * @param _uses number of times this key can be used (in `unlock(..)`)
+     */
+    function grantKey(
+        bytes32 _id,
+        address _to,
+        bool _assignable,
+        uint _expiration,
+        uint _uses
+    ) internal
+    {
+        require(isValidExpiration(_expiration), "Expiration must be in the future");
+
+        keys[_id][_to].exists = true;
+        keys[_id][_to].assignable = _assignable;
+        keys[_id][_to].expiration = _expiration;
+        keys[_id][_to].uses = _uses;
+
+        emit AssignKey(
+            _id,
+            0,
+            _to,
+            _assignable,
+            _expiration,
+            _uses
+        );
+    }
+
+    /**
+     * @dev Grant full capabilities to account (assignable, no expiration, infinite uses)
+     * @param _id lock id
+     * @param _to recipient
+     */
+    function grantFullKey(
+        bytes32 _id,
+        address _to
+    ) internal
+    {
+        grantKey(
+            _id,
+            _to,
+            true,
+            0,
+            0
+        );
+    }
+
+    /**
+     * @dev A convenience modifier that guarantees a condition to be true. eg. `guarantee(unlock('Admin') || unlock('Worker'))`
+     * @param _condition the condition to be met
+     */
+    modifier guarantee(bool _condition) {
+        require(_condition, "Insufficiant permissions");
+        _;
+    }
+
+    /**
+     * @dev unlock a lock if sender has a valid key.
+     * @param _id lock id
+     */
+    function unlock(bytes32 _id) internal returns (bool) {
+        if (unlockable(_id, msg.sender)) {
+            subtractUses(_id, 1);
+            return true;
+        }
+        return false;
+    }
+
+    /* A collection of convinience methods for generating lock ids */
+    function lockId(bytes32 _arg0, bytes32 _arg1) internal returns(bytes32) {
+        return keccak256(abi.encodePacked(_arg0, _arg1));
+    }
+    function lockId(bytes32 _arg0, bytes32 _arg1, bytes32 _arg2) internal returns(bytes32) {
+        return keccak256(abi.encodePacked(_arg0, _arg1, _arg2));
+    }
+    function lockId(bytes32 _arg0, bytes32 _arg1, bytes32 _arg2, bytes32 _arg3) internal returns(bytes32) {
+        return keccak256(abi.encodePacked(_arg0, _arg1, _arg2, _arg3));
+    }
+    function lockId(bytes32 _arg0, bytes32 _arg1, bytes32 _arg2, bytes32 _arg3, bytes32 _arg4) internal returns(bytes32) {
+        return keccak256(abi.encodePacked(_arg0, _arg1, _arg2, _arg3, _arg4));
+    }
+
+    function subtractUses(bytes32 _id, uint _uses) private {
+        Key memory key = keys[_id][msg.sender];
+        if (key.uses > 0) {
+            if (key.uses == _uses) {
+                delete keys[_id][msg.sender];
+            } else {
+                keys[_id][msg.sender].uses = keys[_id][msg.sender].uses.sub(_uses);
+            }
+        }
+    }
+}
